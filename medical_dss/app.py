e871c000227c241e17315440a97e40c56d6cc6ab
Your Name <youremail@example.com>
2026-05-23 15:40:01 +0300

    Remove Flask API, load model directly

--- medical_dss/app.py
+++ medical_dss/app.py
@@ -8,13 +8,13 @@ model = joblib.load('dss_model.pkl')
 st.title("Medical DSS - Prediction App")
 st.write("Enter patient symptoms to get a predicted illness")
 
-#Input fields matching your model
-fever = st.selectbox("Fever?", [0,1], format_func=lambda x: "Yes" if x == 1 else "No")
-cough = st.selectbox("cough?", [0,1], format_func=lambda x: "Yes" if x == 1 else "No")
-shortness_of_breath = st.selectbox("Shortness of breath?", [0,1], format_func=lambda x: "Yes" if x == 1 else"No")
-chest_pain = st.selectbox("Chest pain?", [0,1], format_func=lambda x: "Yes" if x == 1 else "No")
+#Input fields
+fever = st.selectbox("Do you have fever?", [0,1], format_func=lambda x: "Yes" if x == 1 else "No")
+cough = st.selectbox("Do you have cough?", [0,1], format_func=lambda x: "Yes" if x == 1 else "No")
+shortness_of_breath = st.selectbox("Do you have shortness of breath?", [0,1], format_func=lambda x: "Yes" if x == 1 else"No")
+chest_pain = st.selectbox("Do you have chest pain?", [0,1], format_func=lambda x: "Yes" if x == 1 else "No")
 
-if st.button("Predict"):
+if st.button("Predict Illness"):
     input_data = pd.DataFrame([[fever, cough, shortness_of_breath, chest_pain]],
                               columns=['fever', 'cough', 'shortness_of_breath', 'chest_pain'])
     prediction = model.predict(input_data)[0]
